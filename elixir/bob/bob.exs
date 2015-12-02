ae1e5539fd9bd17376280eea706d6b99aaba2c6a
Cedric Charly <cedric.charly@gmail.com>
2015-12-02 12:32:11 -0500

    first commit

---
+++ elixir/bob/bob.exs
@@ -0,0 +1,14 @@
+defmodule Teenager do
+  def hey(input) do
+    cond do
+      String.strip(input) == "" ->
+        "Fine. Be that way!"
+      String.ends_with?(input, "?") ->
+        "Sure."
+      String.upcase(input) == input and String.downcase(input) != input ->
+        "Whoa, chill out!"
+      true ->
+        "Whatever."
+    end
+  end
+end
---
+++ elixir/word-count/word_count.exs
@@ -0,0 +1,21 @@
+defmodule Words do
+  @doc """
+  Count the number of words in the sentence.
+
+  Words are compared case-insensitively.
+  """
+  @spec count(String.t) :: map()
+  def count(sentence) do
+    tokens = String.split(String.replace(String.downcase(sentence), ~r/_/, " ")) 
+    word_dict = build_map(tokens, Map.new())
+  end
+
+  defp build_map([head | tail], map) do
+    map = build_map(tail, map)
+    Map.update(map, head, 1, fn(val) -> val + 1 end)
+  end
+
+  defp build_map([], map) do
+    map
+  end
+end
