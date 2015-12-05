ce74f8d4d67129f0c92e901c7b417fc8f554a6fd
Cedric Charly <cedric.charly@gmail.com>
2015-12-04 22:45:25 -0500

    Refactor elxir/list_ops for readability and better design

--- elixir/list-ops/list_ops.exs
+++ elixir/list-ops/list_ops.exs
@@ -7,99 +7,35 @@ defmodule ListOps do
   # automatically imported) and so shouldn't be used either.
 
   @spec count(list) :: non_neg_integer
-  def count(l) do
-   count_one(l)
-  end
-
-  defp count_one([_ | tail]) do
-    1 + count_one(tail)
-  end
-
-  defp count_one([]) do
-    0
-  end
+  def count([]), do: 0
+  def count([_ | tail]), do: 1 + count(tail)
 
   @spec reverse(list) :: list
-  def reverse(l) do
-    reverse_one(l, [])
-  end
-
-  defp reverse_one([head | tail], list) do
-    list = [head | list]
-    reverse_one(tail, list)
-  end
-
-  defp reverse_one([], list) do
-    list
-  end
+  def reverse(list), do: reverse(list, [])
+  defp reverse([], list), do: list
+  defp reverse([head | tail], list), do: reverse(tail, [head | list])
 
   @spec map(list, (any -> any)) :: list
-  def map(l, f) do
-    map_one(l, f)
-  end
-
-  defp map_one([head | tail], f) do
-    [f.(head) | map_one(tail, f)]
-  end
-
-  defp map_one([], f) do
-    []
-  end
+  def map([], _), do: []
+  def map([head | tail], f), do: [f.(head) | map(tail, f)]
 
   @spec filter(list, (any -> as_boolean(term))) :: list
-  def filter(l, f) do
-    filter_one(l, f)
-  end
-
-  defp filter_one([head | tail], f) do
-    result = filter_one(tail, f)
+  def filter([], _), do: []
+  def filter([head | tail], f) do
+    result = filter(tail, f)
     if f.(head), do: [head | result], else: result
-  end
-
-  defp filter_one([], _) do
-    []
   end
 
   @type acc :: any
   @spec reduce(list, acc, ((any, acc) -> acc)) :: acc
-  def reduce(l, acc, f) do
-    reduce_one(l, acc, f)
-  end
-
-  defp reduce_one([head | tail], acc, f) do
-    acc = f.(head, acc)
-    reduce_one(tail, acc, f)
-  end
-
-  defp reduce_one([], acc, _) do
-    acc
-  end
+  def reduce([head | tail], acc, f), do: reduce(tail, f.(head, acc), f)
+  def reduce([], acc, _), do: acc
 
   @spec append(list, list) :: list
-  def append(a, b) do
-    append_one(reverse(a), b)
-  end
-
-  defp append_one([head | tail], list) do
-    list = [head | list]
-    append_one(tail, list)
-  end
-
-  defp append_one([], list) do
-    list
-  end
+  def append([], list), do: list
+  def append([head | tail], list), do: [head | append(tail, list)]
 
   @spec concat([[any]]) :: [any]
-  def concat(ll) do
-    concat_one(reverse(ll), [])
-  end
-
-  defp concat_one([head | tail], list) do
-    list = reduce(reverse(head), list, &([&1 | &2]))
-    concat_one(tail, list)
-  end
-
-  defp concat_one([], list) do
-    list
-  end
+  def concat([]), do: []
+  def concat([head | tail]), do: append(head, concat(tail))
 end
