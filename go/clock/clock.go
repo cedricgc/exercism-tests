5bdfc6ba762da4e24e7236e2712f1a2ead52f59d
Cedric Charly <cedric.charly@gmail.com>
2016-02-13 18:42:25 -0500

    Changed clock to typedef of uint64

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -12,37 +12,23 @@ const (
 
 const TestVersion = 2
 
-type Clock struct {
-	hour, minute int
-}
+type Clock uint64
 
 func Time(hour, minute int) Clock {
-	minutestoadd := HOUR*hour + minute
-	return Clock{hour: 0, minute: 0}.Add(minutestoadd)
+	minutestoadd := (HOUR*hour + minute) % DAY
+	if minutestoadd < 0 {
+		minutestoadd += DAY
+	}
+	return Clock(minutestoadd)
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%.2d:%.2d", c.hour, c.minute)
+	return fmt.Sprintf("%.2d:%.2d", c/60, c%60)
 }
 
 func (c Clock) Add(minutes int) Clock {
-	total := HOUR*c.hour + c.minute
-	total = total + minutes
-	if total < 0 {
-		total = DAY - (-total)
+	if minutes < 0 {
+		minutes = (minutes%DAY + DAY)
 	}
-
-	hour := int(total / HOUR)
-	hour = hour % DAYHOURS
-	if hour < 0 {
-		hour = DAYHOURS + hour
-	}
-
-	minute := total % HOUR
-	if minute < 0 {
-		minute = HOUR + minute
-		hour = hour - 1
-	}
-
-	return Clock{hour: hour, minute: minute}
+	return (c + Clock(minutes)) % Clock(DAY)
 }
